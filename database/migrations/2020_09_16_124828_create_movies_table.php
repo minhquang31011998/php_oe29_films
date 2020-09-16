0db14f558f454d277a15224a8aec40621212cbb6
MinhQuang <minhquang31011998@gmail.com>
2020-09-16 20:32:47 +0700

    update migration part1

---
+++ database/migrations/2020_09_16_124828_create_movies_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id')->comment('id');
+            $table->string('name')->comment('ten phim');
+            $table->string('name_origin')->comment('ten phim goc');
+            $table->text('description')->comment('mo ta');
+            $table->string('card_cover')->comment('link anh bia');
+            $table->integer('age')->comment('tuoi');
+            $table->boolean('nominations')->default(0)->comment('uu tien');
+            $table->integer('genre')->comment('phan loai');
+            $table->integer('runtime')->comment('thoi luong');
+            $table->integer('release_year')->comment('nam san xuat');
+            $table->integer('quality')->comment('chat luong');
+            $table->integer('country')->comment('quoc gia');
+            $table->integer('rate')->comment('diem danh gia');
+            $table->string('slug')->comment('ten');
+            $table->integer('user_id')->comment('nguoi tao');
+            $table->timestamps();
+            $table->softDeletes()->comment('thoi gian xoa');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
---
+++ database/migrations/2020_09_16_124912_create_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id')->comment('id');
+            $table->string('name')->comment('ten the tags');
+            $table->string('slug')->nullable()->comment('duong dan');
+            $table->timestamps();
+            $table->softDeletes()->comment('thoi gian xoa');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
