715477128051f8e18c60da919f1de14859b72bfa
MinhQuang <minhquang31011998@gmail.com>
2020-10-09 10:36:47 +0700

    update video

--- app/Http/Controllers/Backend/VideoController.php
+++ app/Http/Controllers/Backend/VideoController.php
@@ -9,6 +9,8 @@ class VideoController extends Controller
 {
     public function index()
     {
+        session(['previous-url' => request()->url()]);
+
         return view('backend.videos.index');
     }
 
@@ -40,7 +42,7 @@ class VideoController extends Controller
                 return '<div class="main__table-text">' . $video['title'] . '</div>';
             })
             ->editColumn('status', function ($video) {
-                if (config('config.status_active')) {
+                if ($video['status'] == config('config.status_active')) {
                     return '<div class="main__table-text main__table-text--green">' . trans('active') . '</div>';
                 } else {
                     return '<div class="main__table-text main__table-text--red">' . trans('hidden') . '</div>';
@@ -106,5 +108,140 @@ class VideoController extends Controller
         $source->save();
     }
 
+    public function getSources(Request $request)
+    {
+        $sources = Source::select('id', 'source_key', 'prioritize', 'channel_id')
+            ->where('video_id', '=', $request->get('videoId'))
+            ->orderByRaw('prioritize ASC');
 
+        return DataTables::of($sources->get()->toArray())
+            ->addColumn('action', function ($source) {
+                return
+                    '<div class="main__table-btns">
+                        <button class="main__table-btn main__table-btn--edit edit-source" title="Edit" data-source="' . $source['id'] . '">
+                            <i class="icon ion-ios-create"></i>
+                        </button>
+
+                        <button class="main__table-btn main__table-btn--delete delete_source" value="' . $source['id'] . '" title="Delete" onclick="deleteSource(' . $source['id'] . ')">
+                            <i class="icon ion-ios-trash"></i>
+                        </button>
+                    </div>';
+            })
+            ->editColumn('id', function ($source) {
+                return '<div class="main__table-text">' . $source['id'] . '</div>';
+            })
+            ->editColumn('source_key', function ($source) {
+                return '<div class="main__table-text">' . $source['source_key'] . '</div>';
+            })
+            ->editColumn('prioritize', function ($source) {
+                return '<div class="main__table-text">' . $source['prioritize'] . '</div>';
+            })
+            ->editColumn('channel_id', function ($source) {
+                $channel = Channel::select('title')->where('id', '=', $source['channel_id'])->first();
+
+                return '<div class="main__table-text">' . $channel['title'] . '</div>';
+            })
+            ->rawColumns(['id', 'source_key', 'prioritize', 'channel_id', 'action'])
+            ->make(true);
+    }
+
+    public function edit($videoId)
+    {
+        try {
+            $video = Video::findOrFail($videoId);
+            $channels = Channel::get();
+            if ($video->movie_id != null) {
+                $video->movie = $video->movie()->first();
+            } else {
+                $video->movie = null;
+            }
+
+            return view('backend.videos.edit')->with([
+                'video'=>$video->toArray(),
+                'channels'=>$channels
+            ]);
+        } catch (ModelNotFoundException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function update(VideoUpdateRequest $request, $videoId)
+    {
+        try {
+            $video = Video::findOrFail($videoId);
+
+            $video->title = $request->get('title');
+            $video->tags = $request->get('tags');
+            $video->description = $request->get('description');
+            $video->chap = $request->get('chap');
+            if ($video->playlist_id != null && $request->has('chap') && $video->playlist_id != null) {
+                $playlistVideos = Playlist::findOrFail($video->playlist_id)
+                    ->videos()
+                    ->where('id', '!=', $request->get('videoId'))
+                    ->get()
+                    ->sortBy('chap');
+                $this->sortChap($request->get('chap'), $playlistVideos);
+            }
+            $video->user_id = 1;
+            $oldVideo = Video::where('slug', $video->slug)->first();
+            if ($oldVideo == null) {
+                $video->slug = Str::slug($request->get('title'));
+            } else {
+                $video->slug = $oldVideo->slug . '-' . Str::random(3);
+            }
+            $video->save();
+
+            Session::flash('status', 'new');
+
+            return redirect(session('previous-url'));
+        } catch (ModelNotFoundException $e) {
+            return redirect()->back()->withErrors(['msg', $e->getMessage()]);
+        }
+    }
+
+    public function sortChap($chap, $playlistVideos)
+    {
+        $i = $chap;
+        foreach ($playlistVideos as $playlistVideo) {
+            if ($playlistVideo->chap >= $chap) {
+                $playlistVideo->chap = ++$i;
+                $playlistVideo->save();
+            }
+        }
+    }
+
+    public function changeStatus($sourceId)
+    {
+        try {
+            $video = Video::findOrFail($sourceId);
+            if ($video->status == config('config.status_active')) {
+                $video->status = config('config.status_hidden');
+            } else {
+                $video->status = config('config.status_active');
+            }
+            $video->user_id = 1;
+            $video->save();
+
+            return $video;
+        } catch (ModelNotFoundException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function detach($videoId)
+    {
+        $video = Video::findOrFail($videoId);
+        $video->playlist_id = null;
+        $video->movie_id = null;
+        $video->save();
+
+        return redirect()->back();
+    }
+
+    public function destroy($videoId)
+    {
+        $video = Video::destroy($videoId);
+
+        return redirect()->route('backend.video.index');
+    }
 }
--- resources/assets/backend/js/crudvideo.js
+++ resources/assets/backend/js/crudvideo.js
@@ -1,1 +1,21 @@
-$('#channels').select2();
+$(document).ready(function () {
+    $('.channels').select2();
+    $('#channels').select2();
+    $('#status').on('click', function () {
+        $.ajax({
+            type: "POST",
+            url: '/admin/video/changeStatus/' + $('#videoId').val(),
+            dataType: 'json',
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success: (response) => {
+                if (response.status == 1) {
+                    $(this).html('<i class="fa fa-unlock-alt" aria-hidden="true" data-toggle="tooltip" title="Activate"></i>');
+                } else {
+                    $(this).html('<i class="icon ion-ios-lock" data-toggle="tooltip" title="Hide"></i>');
+                }
+            }
+        });
+    })
+});
