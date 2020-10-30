d7897dc9aa07a8779696963a191c3a12879a0758
MinhQuang <minhquang31011998@gmail.com>
2020-10-30 11:01:35 +0700

    test rate controller

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Rate\RateRepository;
+use App\Repositories\Rate\RateRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(
+            RateRepositoryInterface::class,
+            RateRepository::class
+        );
     }
 
     /**
